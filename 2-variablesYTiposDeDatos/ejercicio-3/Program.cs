b51cca2fbb247e04b4f194accb7b309ee02c9794
Claret Devigne <57070276+claretdevigne@users.noreply.github.com>
2023-03-14 17:51:23 -0500

    Create the "ejercicio-3" folder

---
+++ 2-variablesYTiposDeDatos/ejercicio-3/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("¿Número 2 es mayor o igual a 18?: " + (2 >= 18));
+System.Console.WriteLine("'a' es igual a 'a': " + (a == a));
+
+bool condicion1 = 2 > 1;
+bool condicion2 = 3 == 3;
+
+bool condicion3 = 10 > 8;
+bool condicion4 = 10 < 8;
