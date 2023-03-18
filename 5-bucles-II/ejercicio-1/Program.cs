35f10aeda148e2830da0e2bd4aba0e7ad5d23398
Claret Devigne <57070276+claretdevigne@users.noreply.github.com>
2023-03-18 14:41:58 -0500

    Create "5-bucles-II" folder

---
+++ 5-bucles-II/ejercicio-1/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Clear();
+System.Console.WriteLine("### 5 BUCLES II - EJERCICIO 1 ###\n");
+System.Console.WriteLine("Ingrese su nombre: ");
+string name = Console.ReadLine();
+System.Console.WriteLine("Ingrese su email: ");
+string email = Console.ReadLine();
+System.Console.WriteLine("¿Posée cupon? (Sí | No): ");
+string cupon = Console.ReadLine();
+
+System.Console.WriteLine("\nEl costo de su producto es de 100$.");
+if (cupon == "Sí" || cupon == "Si" || cupon == "sí" || cupon == "si") {
+  System.Console.WriteLine("Descuento de cupón 50%");
+  System.Console.WriteLine("Total a pagar: 50$");
+} else if (cupon == "No" || cupon == "no") {
+  System.Console.WriteLine("--- Total a pagar: 100$ ---");
+}
---
+++ 5-bucles-II/ejercicio-2/Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.Clear();
+System.Console.WriteLine("### Bucles II - Ejercicio 2 ###\n");
+System.Console.WriteLine("Por favor ingresa una opción");
+System.Console.WriteLine("1) C#");
+System.Console.WriteLine("2) Java");
+System.Console.WriteLine("3) Python");
+System.Console.WriteLine("4) Javascript");
+System.Console.WriteLine("5) Typescript");
+System.Console.Write("\nIngrese su opción: ");
+string option = Console.ReadLine();
+System.Console.WriteLine();
+
+switch (option) {
+  case "1":
+    System.Console.WriteLine("Usted seleccionó C#.");
+    break;
+
+  case "2":
+    System.Console.WriteLine("Usted seleccionó Java.");
+    break;
+
+  case "3":
+    System.Console.WriteLine("Usted seleccionó Python.");
+    break;
+
+  case "4":
+    System.Console.WriteLine("Usted seleccionó Javascript.");
+    break;
+
+  case "5":
+    System.Console.WriteLine("Usted seleccionó Typescript.");
+    break;
+}
